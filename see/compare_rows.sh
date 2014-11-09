47f9dfb5e2cfc636cbc25261666a1cffc9130888
Yuu Jinnai <ddyuudd@gmail.com>
2014-11-09 18:59:17 +0900

    Experiment: 1109185856 hdastar easy_instances 6 1000000 1000000 0 easy.6.threads.possibly.new.finding

--- see/compare_rows.sh
+++ see/compare_rows.sh
@@ -11,11 +11,11 @@ echo "title = "
 read title
 
 gnuplot<<EOF
-  set terminal postscript enhanced
-  plot "$dat1" using 1:$column w l title "$dat1"
-  replot "$dat2" using 1:$column w l title "$dat2"
-  replot "$dat3" using 1:$column w l title "$dat3"
-  replot "$dat4" using 1:$column w l title "$dat4"
+  set terminal postscript enhanced color
+  plot "$dat1" using 1:$column w l linetype 1 linecolor 1 title "$dat1"
+  replot "$dat2" using 1:$column w l linetype 1 linecolor 2 title "$dat2"
+  replot "$dat3" using 1:$column w l linetype 1 linecolor 3 title "$dat3"
+  replot "$dat4" using 1:$column w l linetype 1 linecolor 4 title "$dat4"
   set output "analysis/$title.eps"
-  replot "$dat5" using 1:$column w l title "$dat5"
+  replot "$dat5" using 1:$column w l linetype 1 linecolor 5 title "$dat5"
 EOF
