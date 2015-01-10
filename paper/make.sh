5e8ac3994f16d83fd58334eeefcccc112e403633
Yuu Jinnai <ddyuudd@gmail.com>
2015-01-10 19:49:53 +0900

    autocommit 01/10_19:49

--- paper/make.sh
+++ paper/make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-date=`date +%m/%d\ %H:%M`
+date=`date +%m/%d_%H:%M`
 
 git commit -m "autocommit $date" paper.tex b.bib make.sh
 #git push
@@ -10,9 +10,9 @@ platex -shell-escape paper.tex
 pbibtex paper.aux
 platex -shell-escape paper.tex
 dvipdfmx paper.dvi
-cp paper.pdf "2015_陣内佑_マルチコア環境における並列A*探索の探索オーバーヘッドの定性的な解析とアルゴリズムの再評価(20150107).pdf"
+cp paper.pdf "2015_陣内佑_マルチコア環境における並列A*探索の探索オーバーヘッドの定性的な解析とアルゴリズムの再評価($date).pdf"
 
-cp "2015_陣内佑_マルチコア環境における並列A*探索の探索オーバーヘッドの定性的な解析とアルゴリズムの再評価(20150107).pdf" /home/yuu/Dropbox/
+cp "2015_陣内佑_マルチコア環境における並列A*探索の探索オーバーヘッドの定性的な解析とアルゴリズムの再評価($date).pdf" /home/yuu/Dropbox/
 
 evince paper.pdf
 
