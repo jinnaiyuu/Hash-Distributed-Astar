769c5b46249f8cccec4308db29d445eafb312758
Yuu Jinnai <ddyuudd@gmail.com>
2015-01-11 16:26:03 +0900

    autocommit 01-11

--- paper/make.sh
+++ paper/make.sh
@@ -7,6 +7,7 @@ git commit -m "autocommit $date" paper.tex b.bib make.sh
 #git push
 
 platex -shell-escape paper.tex
+makeindex paper
 platex -shell-escape paper.tex
 pbibtex paper.aux
 platex -shell-escape paper.tex
