e5eff6b084fd1975d39246cee9adbadfa5d6b6b8
Yuu Jinnai <ddyuudd@gmail.com>
2015-01-17 21:35:14 +0900

    autocommit 0117

--- paper/make.sh
+++ paper/make.sh
@@ -13,7 +13,7 @@ uplatex -shell-escape paper.tex
 dvipdfmx paper.dvi
 cp paper.pdf "2015${date}_陣内佑_マルチコア環境における並列A*探索の探索オーバーヘッドの定性的な解析とアルゴリズムの再評価.pdf"
 
-cp ../paper/* ~/Dropbox/my_papers/paper
+cp -r ../paper/* ~/Dropbox/my_papers/paper
 
 evince paper.pdf
 
