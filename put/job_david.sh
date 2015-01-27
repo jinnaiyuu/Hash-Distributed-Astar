770974e74273f44fd7d5dd50fc1fbc2f23bb991c
Yuu Jinnai <ddyuudd@gmail.com>
2015-01-27 21:01:29 +0900

    Experiment: 0127210127 difficult_instances 24 hdastar 1 1 0 closed-8503057

--- put/job_david.sh
+++ put/job_david.sh
@@ -27,19 +27,13 @@ mem="32gbmem"
 # algorithm.threadnumber.corenumber.memorysize.parameterforalgorithm.parametertoanalyze
 
 # For HDASTAR example
-if [ "$algname" = "hdastar" ]
-then
-    paramname1="incomebufsize"
-    paramname2=".outgobufsize"
-    paramname3=".abstraction"
-fi
 
 
-JOB_ARRAY_ID=`qsub -t 1-$problem_size -l nodes=1:ppn=8,walltime=02:00:00 -N $algname.${problem_type}.${thread_number}threads.${mem}.${paramname1}${param1}${paramname2}${param2}${paramname3}${param3}.${time} -j oe -v time=$time,domain_name=$domain_name,algname=$algname,thread_number=$thread_number,problem_type=$problem_type,param1=$param1,param2=$param2,param3=$param3 ./run.sh`
+JOB_ARRAY_ID=`qsub -t 1-$problem_size -l nodes=1:ppn=8,walltime=02:00:00 -N $algname.${problem_type}.${thread_number}threads.${mem}.${param1}${param2}${param3}.${time} -j oe -v time=$time,domain_name=$domain_name,algname=$algname,thread_number=$thread_number,problem_type=$problem_type,param1=$param1,param2=$param2,param3=$param3 ./run.sh`
 
 JOB_NUMBER=`echo $JOB_ARRAY_ID | awk -F "." '{print $1}'` 
 
-echo $time $JOB_NUMBER $algname $problem_size $problem_type $thread_number $mem $param1 $param2 $comment  >> job_list.dat
+echo $time $JOB_NUMBER $algname $problem_size $problem_type $thread_number $mem $param1 $param2 $param3 $comment  >> job_list.dat
 
 # Not working. Not sure how to fix it.
 #qsub -W depend:afterokarray:$JOB_ARRAY_ID[] -M ddyuudd@gmail.com -m ae ./mail.sh
